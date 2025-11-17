20ce8acf3c0b8ebe6fdb71ab8994507259ce5acf
Calvin Prajogo <calprajogo@gmail.com>
2025-11-17 14:13:11 -0600

    Import AuthProvider to App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { ScheduleProvider } from "./src/context/ScheduleContext";
+import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
     return (
