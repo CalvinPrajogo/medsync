3598d204d6d2450f16520a83bff14e15bdd2a908
Calvin Prajogo <calprajogo@gmail.com>
2025-11-20 13:30:11 -0600

    Implement create account and logout features

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
+import SignUpScreen from "../screens/SignUpScreen";
 import HomeScreen from "../screens/HomeScreen";
 import DatabaseScreen from "../screens/DatabaseScreen";
 import MedicineDetailScreen from "../screens/MedicineDetailScreen";
@@ -23,6 +24,7 @@ const AppNavigator = () => {
                 }}
             >
                 <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Database" component={DatabaseScreen} />
                 <Stack.Screen name="MedicineDetail" component={MedicineDetailScreen} />
